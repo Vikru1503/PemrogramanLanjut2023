06d2b33ed706ba5e51e0dad4706cfc796f697677
Vikru1503 <153990070+Vikru1503@users.noreply.github.com>
2024-07-24 22:42:29 +0800

    Tugas UAS Selesai

---
+++ UAS/src/vikrusw/Main.java
@@ -0,0 +1,9 @@
+package vikrusw;
+import gui.*;
+public class Main {
+    public static void main(String[] args) {
+    Register register = new Register();
+    // setLocationRelative(null)
+
+    }
+}
