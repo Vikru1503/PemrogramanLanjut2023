93ab18d7af598065f4920444260d5c16d124cf21
Vikru1503 <153990070+Vikru1503@users.noreply.github.com>
2024-07-24 23:23:42 +0800

    Delete UAS directory

--- UAS/src/vikrusw/Main.java
+++
@@ -1,9 +0,0 @@
-package vikrusw;
-import gui.*;
-public class Main {
-    public static void main(String[] args) {
-    Register register = new Register();
-    // setLocationRelative(null)
-
-    }
-}
