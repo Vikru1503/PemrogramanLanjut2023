eb64957c15e90eb31f6693e91a25a9d98f339b33
Vikru1503 <153990070+Vikru1503@users.noreply.github.com>
2024-07-24 23:50:54 +0800

    Tugas UAS

---
+++ UAS Pemrograman/src/percobaanke2/Main.java
@@ -0,0 +1,9 @@
+package percobaanke2;
+import gui.*;
+public class Main {
+    public static void main(String[] args) {
+    Register register = new Register();
+    // setLocationRelative(null)
+
+    }
+}
